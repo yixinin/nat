c59f71f1c05d06b9dc99317752c623a318b80eaf
eason ran <eason.ran@splashtop.com>
2023-09-01 02:32:27 +0800

    update

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strconv"
 	"time"
 )
 
 var (
 	Type string
+	lp   int
 )
 
 func main() {
 	flag.StringVar(&Type, "t", "", "run type")
+	flag.IntVar(&lp, "p", 8888, "local addr")
 	flag.Parse()
 
 	var ctx, cancel = context.WithCancel(context.Background())
@@ -26,54 +29,12 @@ func main() {
 	case "client":
 		err := client(ctx)
 		fmt.Println(err)
-	case "server":
-		err := server(ctx)
-		fmt.Println(err)
-	}
-}
-
-func server(ctx context.Context) error {
-	laddr := &net.UDPAddr{
-		Port: 8801,
-	}
-	raddr := &net.UDPAddr{
-		Port: 8888,
-		IP:   net.IPv4(114, 115, 218, 1),
-	}
-
-	conn2, err := net.ListenUDP("udp", laddr)
-	if err != nil {
-		return err
-	}
-	_, err = conn2.WriteToUDP([]byte("server"), raddr)
-	if err != nil {
-		return err
-	}
-	go func() {
-		tk := time.NewTicker(time.Second)
-		for range tk.C {
-			n, err := conn2.WriteToUDP([]byte("::"), raddr)
-			fmt.Println("tick to stun", raddr, n, err)
-		}
-	}()
-	var buf = make([]byte, 512)
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			n, raddr, err := conn2.ReadFromUDP(buf)
-			if err != nil {
-				return err
-			}
-			fmt.Println("server recv", raddr, string(buf[:n]))
-		}
 	}
 }
 
 func client(ctx context.Context) error {
 	laddr := &net.UDPAddr{
-		Port: 8802,
+		Port: lp,
 	}
 
 	conn, err := net.ListenUDP("udp", laddr)
@@ -84,19 +45,15 @@ func client(ctx context.Context) error {
 		Port: 8888,
 		IP:   net.IPv4(114, 115, 218, 1),
 	}
-	n, err := conn.WriteToUDP([]byte("client"), raddr)
+	n, err := conn.WriteToUDP([]byte(strconv.Itoa(lp)), raddr)
 	fmt.Println("send to stun", raddr, n, err)
-	go func() {
-		raddr := &net.UDPAddr{
-			Port: 8888,
-			IP:   net.IPv4(114, 115, 218, 1),
-		}
+	go func(raddr *net.UDPAddr) {
 		tk := time.NewTicker(time.Second)
 		for range tk.C {
 			n, err := conn.WriteToUDP([]byte("::"), raddr)
 			fmt.Println("send to stun", raddr, n, err)
 		}
-	}()
+	}(raddr)
 
 	var buf = make([]byte, 512)
 	for {
@@ -105,22 +62,27 @@ func client(ctx context.Context) error {
 			return err
 		}
 		fmt.Println("client recv", raddr, string(buf[:n]))
-		switch string(buf[:n]) {
-		case "::":
+		switch n {
+		case 2:
+		case 0:
 		default:
-			raddr = new(net.UDPAddr)
-			err = json.Unmarshal(buf[:n], raddr)
-			if err != nil {
-				return err
-			}
-			fmt.Println("try to connect", raddr)
-			go func(raddr *net.UDPAddr) {
-				tk := time.NewTicker(time.Second)
-				for range tk.C {
-					n, err = conn.WriteToUDP([]byte("hello server, I'm client from stun"), raddr)
-					fmt.Println("send to server", raddr, n, err)
+			switch buf[0] {
+			case '{':
+				raddr = new(net.UDPAddr)
+				err = json.Unmarshal(buf[:n], raddr)
+				if err != nil {
+					return err
 				}
-			}(raddr)
+				fmt.Println("try to connect", raddr)
+				go func(raddr *net.UDPAddr) {
+					tk := time.NewTicker(time.Second)
+					for range tk.C {
+						n, err = conn.WriteToUDP([]byte("hello server, I'm client from stun"), raddr)
+						fmt.Println("send to server", raddr, n, err)
+					}
+				}(raddr)
+			default:
+			}
 		}
 	}
 }
@@ -133,7 +95,7 @@ func stun(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	var serverAddr, clientAddr *net.UDPAddr
+	var m = map[string]*net.UDPAddr{}
 	for {
 		var buf = make([]byte, 512)
 		select {
@@ -145,32 +107,37 @@ func stun(ctx context.Context) error {
 				return err
 			}
 			fmt.Println("recv", string(buf[:n]), "from", raddr)
-			data, err := json.Marshal(raddr)
-			if err != nil {
-				return err
-			}
-			switch string(buf[:n]) {
-			case "server":
-				serverAddr = raddr
-				if clientAddr != nil {
-					_, err = conn.WriteTo(data, clientAddr)
-					if err != nil {
-						return err
-					}
-				}
-			case "client":
-				clientAddr = raddr
-				if serverAddr != nil {
-					_, err = conn.WriteTo(data, serverAddr)
-					if err != nil {
-						return err
-					}
-				}
-			default:
+
+			switch s := string(buf[:n]); s {
+			case "::":
 				_, err = conn.WriteTo(buf[:n], raddr)
 				if err != nil {
 					return err
 				}
+			default:
+				if len(m) == 1 {
+					for _, addr := range m {
+						data, err := json.Marshal(raddr)
+						if err != nil {
+							return err
+						}
+						_, err = conn.WriteTo(data, addr)
+						if err != nil {
+							return err
+						}
+
+						data, err = json.Marshal(addr)
+						if err != nil {
+							return err
+						}
+						_, err = conn.WriteTo(data, raddr)
+						if err != nil {
+							return err
+						}
+					}
+				}
+				m[string(buf[:n])] = raddr
+
 			}
 		}
 	}
