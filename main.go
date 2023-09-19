6fb1a10822d9f8a24ebd3fb715172f4b0bc9a2bc
eason <yixinin@outlook.com>
2023-09-19 20:11:33 +0800

    - update

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	flag.BoolVar(&backend, "b", false, "backend server")
 	flag.BoolVar(&frontend, "f", false, "frontend cient")
 	flag.BoolVar(&ddns, "ddns", false, "run ddns server")
-	flag.StringVar(&stunAddr, "stun", "114.115.218.1:8080", "stun server addr")
+	flag.StringVar(&stunAddr, "stun", "114.115.218.1:2023", "stun server addr")
 	flag.StringVar(&localAddr, "localAddr", "", "listen addr")
 	flag.StringVar(&fqdn, "fqdn", "", "fqdn")
 	flag.Parse()
